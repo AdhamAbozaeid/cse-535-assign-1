b1e913c52038e55a3fd87c3aede84ee11bfe73fa
Adham Abozaeid <adham.abozaeid@gmail.com>
2019-08-03 19:09:46 -0700

    Loading data from DB file and returning it from Patient.loadPatientData
    Signed-off-by: Adham Abozaeid <adham.abozaeid@gmail.com>

--- lalwani/app/src/main/java/com/example/a5/Patient.java
+++ lalwani/app/src/main/java/com/example/a5/Patient.java
@@ -1,9 +1,14 @@
 package com.example.a5;
 
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
+
+import java.sql.Statement;
+
 
 public class Patient {
     private String mName;
@@ -43,6 +48,10 @@ public class Patient {
         }
     }
 
+    protected void finalize() {
+        mDB.close();
+    }
+
     public void addSamples(int timestamps[], float x[], float y[], float z[]) {
         mDB.beginTransaction();
         try {
@@ -61,5 +70,42 @@ public class Patient {
         } finally {
             mDB.endTransaction();
         }
+    }
+
+    public PatientData[] loadPatientData(){
+        mDB.beginTransaction();
+        PatientData patientData[] = null;
+
+        SQLiteStatement statement = mDB.compileStatement("select * from "+ mTableName);
+        Cursor cursor;
+        cursor = mDB.rawQuery("select * from " + mTableName, null);
+
+        try {
+            cursor.moveToFirst();
+            patientData = new PatientData[cursor.getCount()];
+
+            for(int i=0 ;i< cursor.getCount(); i++) {
+                patientData[i] = new PatientData();
+                patientData[i].timestamp = cursor.getInt(0);
+                patientData[i].x = cursor.getFloat(1);
+                patientData[i].y = cursor.getFloat(2);
+                patientData[i].z = cursor.getFloat(3);
+                cursor.moveToNext();
+            }
+            return patientData;
+        } catch (SQLiteException e) {
+            //report problem
+            Log.e("DB", e.getMessage());
+        } finally {
+            cursor.close();
+        }
+        return patientData;
+    }
+
+    public class PatientData{
+        public int timestamp;
+        public float x;
+        public float y;
+        public float z;
     }
 }
